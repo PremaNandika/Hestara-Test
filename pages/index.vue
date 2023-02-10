471a6d42195a7b280aba575967ec3b95711e9a70
PremaNandika <nandika183@gmail.com>
2023-02-10 19:57:11 +0800

    :sparkles: create nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- <Tutorial /> -->
+  <p class="text-6xl font-bold">Hello World</p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
